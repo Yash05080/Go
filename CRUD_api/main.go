dd86c9e39bf76e8398c7545e7569266fd2a3dee9
Yash05080 <yash05080@gmail.com>
2025-03-03 23:51:22 +0530

    web server finish

---
+++ CRUD_api/main.go
@@ -0,0 +1,32 @@
+package main
+
+import {
+	"fmt"
+	"log"
+	"net/http"
+	"encoding/json"
+	"math/rand"
+	"strconv"
+	"github/gorilla/mux"
+}
+
+type Movie struct{
+	ID string `json"ID"`
+	Isbn string `"json"isbn"`
+	Title string `json"title"`
+	Director string `json"director"`
+	
+}
+
+type Director struct{
+	Firstname string `json"firstname"`
+	Lastname string `json"lastname"`
+}
+
+var movies []Movie
+
+func main(){
+	r := mux.NewRouter()
+
+
+}
